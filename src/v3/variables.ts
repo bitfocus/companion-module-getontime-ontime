87499367a936c9de2b59197ef4d6af548ae1c945
arc-alex <ac@omnivox.dk>
2024-04-09 14:41:39 +0200

    comments matching up variables and ontime store path

--- src/v3/variables.ts
+++ src/v3/variables.ts
@@ -3,37 +3,20 @@ import { variableId } from '../enums'
 
 export function variables(): CompanionVariableDefinition[] {
 	const variables: CompanionVariableDefinition[] = [
-		{
-			name: 'Current time of event in milliseconds',
-			variableId: variableId.TimerTotalMs,
-		},
-		{
-			name: 'State of event (Running, Paused, Stopped, Roll)',
-			variableId: variableId.PlayState,
-		},
-		{
-			name: 'ID of current event',
-			variableId: variableId.IdNow,
-		},
-		{
-			name: 'ID of next event',
-			variableId: variableId.IdNext,
-		},
+		//clock
 		{
 			name: 'Clock (hh:mm:ss)',
 			variableId: variableId.Clock,
 		},
+		//timer.addedTime
 		{
-			name: 'Start of event (hh:mm:ss)',
-			variableId: variableId.TimerStart,
-		},
-		{
-			name: 'Expected finish of event (hh:mm:ss)',
-			variableId: variableId.TimerFinish,
-		},
-		{
-			name: 'Time added to current event',
+			name: 'Time added to current event (hh:mm:ss)',
 			variableId: variableId.TimerAdded,
+		},
+		//timer.current
+		{
+			name: 'Current time of event in milliseconds',
+			variableId: variableId.TimerTotalMs,
 		},
 		{
 			name: 'Current time of event (hh:mm:ss)',
@@ -55,65 +38,116 @@ export function variables(): CompanionVariableDefinition[] {
 			name: 'Current event state Seconds',
 			variableId: variableId.TimeS,
 		},
+		//timer.duration
+		//timer.elapsed
+		//timer.expectedFinish
 		{
-			name: 'Title of current event',
-			variableId: variableId.TitleNow,
+			name: 'Expected finish of event (hh:mm:ss)',
+			variableId: variableId.TimerFinish,
 		},
+		//timer.finishedAt
+		//timer.playback
 		{
-			name: 'Note of current event',
-			variableId: variableId.NoteNow,
+			name: 'State of event (Running, Paused, Stopped, Roll)',
+			variableId: variableId.PlayState,
 		},
+		//timer.secondaryTimer
+		//timer.startedAt
 		{
-			name: 'Cue of current event',
-			variableId: variableId.CueNow,
+			name: 'Start of event (hh:mm:ss)',
+			variableId: variableId.TimerStart,
 		},
-		{
-			name: 'Title of next event',
-			variableId: variableId.TitleNext,
-		},
-		{
-			name: 'Note of next event',
-			variableId: variableId.NoteNext,
-		},
-		{
-			name: 'Cue of next event',
-			variableId: variableId.CueNext,
-		},
+		//onAir
 		{
 			name: 'On Air',
 			variableId: variableId.OnAir,
 		},
+		//message.timer.text
 		{
 			name: 'Timer Message',
 			variableId: variableId.TimerMessage,
 		},
+		//message.public.text
 		{
 			name: 'Public Message',
 			variableId: variableId.PublicMessage,
 		},
+		//message.lower.text
 		{
 			name: 'Lower thirds Message',
 			variableId: variableId.LowerMessage,
 		},
+		//message.timer.visible
 		{
 			name: 'Timer Message Visible',
 			variableId: variableId.TimerMessageVisible,
 		},
+		//message.public.visible
 		{
 			name: 'Public Message Visible',
 			variableId: variableId.PublicMessageVisible,
 		},
+		//message.lower.visible
 		{
 			name: 'Lower thirds Message Visible',
 			variableId: variableId.LowerMessageVisible,
 		},
+		//message.timer.blackout
 		{
 			name: 'Timer Blackout',
 			variableId: variableId.TimerBlackout,
 		},
+		//message.timer.blink
 		{
 			name: 'Timer Blinking',
 			variableId: variableId.TimerBlink,
+		},
+		//runtime.numEvents
+		//runtime.selectedEventIndex
+		//runtime.offset
+		//runtime.plannedStart
+		//runtime.actualStart
+		//runtime.plannedEnd
+		//runtime.expectedEnd
+		//eventNow.id
+		{
+			name: 'ID of current event',
+			variableId: variableId.IdNow,
+		},
+		//eventNow.title
+		{
+			name: 'Title of current event',
+			variableId: variableId.TitleNow,
+		},
+		//eventNow.note
+		{
+			name: 'Note of current event',
+			variableId: variableId.NoteNow,
+		},
+		//eventNow.cue
+		{
+			name: 'Cue of current event',
+			variableId: variableId.CueNow,
+		},
+		//eventNext.di
+		{
+			name: 'ID of next event',
+			variableId: variableId.IdNext,
+		},
+		//eventNext.title
+		{
+			name: 'Title of next event',
+			variableId: variableId.TitleNext,
+		},
+		//eventNext.note
+		{
+			name: 'Note of next event',
+			variableId: variableId.NoteNext,
+		},
+		//eventNext.cue
+		{
+			name: 'Cue of next event',
+			variableId: variableId.CueNext,
 		},
 	]
 
