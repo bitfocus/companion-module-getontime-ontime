824fe389c1422e2b5f99450280fa5c29e5a160e2
arc-alex <ac@omnivox.dk>
2024-08-06 14:29:59 +0200

    remove segments and allow user defined colours

--- src/v3/feedbacks/progress.ts
+++ src/v3/feedbacks/progress.ts
@@ -2,6 +2,7 @@ import { CompanionFeedbackDefinition, combineRgb } from '@companion-module/base'
 import { OntimeV3 } from '../ontimev3'
 import { feedbackId } from '../../enums'
 import { graphics } from 'companion-module-utils'
+import { TimerPhase } from '../ontime-types'
 
 export function createProgressFeedbacks(ontime: OntimeV3): { [id: string]: CompanionFeedbackDefinition } {
 	return {
@@ -9,57 +10,49 @@ export function createProgressFeedbacks(ontime: OntimeV3): { [id: string]: Compa
 			type: 'advanced',
 			name: 'Progressbar',
 			description: 'Progressbar indicating the main timer progression',
-			options: [],
+			options: [
+				{ type: 'colorpicker', id: 'normal', label: 'Normal', default: combineRgb(207, 207, 207) },
+				{ type: 'colorpicker', id: 'warning', label: 'Warning', default: combineRgb(255, 171, 51) },
+				{ type: 'colorpicker', id: 'danger', label: 'Danger', default: combineRgb(237, 51, 51) },
+			],
 			callback: (feedback) => {
 				if (!feedback.image) {
 					return {}
 				}
 
 				const { current, duration } = ontime.state.timer
-				if (duration === null) {
-					const options: graphics.OptionsBar = {
-						width: feedback.image.width,
-						height: feedback.image.height,
-						colors: [
-							{
-								size: 100,
-								color: combineRgb(207, 207, 207),
-								background: combineRgb(54, 54, 54),
-								backgroundOpacity: 255,
-							},
-						],
-						barLength: feedback.image.width,
-						barWidth: 10,
-						value: 0,
-						type: 'horizontal',
-						offsetX: 0,
-						offsetY: feedback.image.height - 10,
-						opacity: 255,
-					}
+				const { phase } = ontime.state.timer
+				const val = current !== null && duration !== null ? (1 - current / duration) * 100 : 0
 
-					return {
-						imageBuffer: graphics.bar(options),
-					}
+				let colour = 0
+
+				const { normal, warning, danger } = feedback.options as { [key: string]: number }
+
+				switch (phase) {
+					case TimerPhase.None:
+					case TimerPhase.Default:
+					case TimerPhase.Pending:
+						colour = normal
+						break
+					case TimerPhase.Warning:
+						colour = warning
+						break
+					case TimerPhase.Danger:
+					case TimerPhase.Overtime:
+						colour = danger
+						break
 				}
-
-				const { timeDanger, timeWarning } = ontime.state?.eventNow ?? { timeDanger: 0, timeWarning: 0 }
-				const val = current !== null ? (1 - current / duration) * 100 : 0
-
-				const danger = (timeDanger / duration) * 100
-				const warn = (timeWarning / duration) * 100 - danger
 
 				const options: graphics.OptionsBar = {
 					width: feedback.image.width,
 					height: feedback.image.height,
 					colors: [
 						{
-							size: 100 - warn - danger,
-							color: combineRgb(207, 207, 207),
-							background: combineRgb(54, 54, 54),
-							backgroundOpacity: 255,
+							size: 100,
+							color: colour,
+							background: colour,
+							backgroundOpacity: 150,
 						},
-						{ size: warn, color: combineRgb(255, 171, 51), background: combineRgb(64, 47, 23), backgroundOpacity: 255 },
-						{ size: danger, color: combineRgb(255, 0, 0), background: combineRgb(60, 23, 23), backgroundOpacity: 255 },
 					],
 					barLength: feedback.image.width,
 					barWidth: 10,
